0c568ff4b8d2d5036409f3c2d3ad78c5d4a10dbb
Samantha <contact@samanthanguyen.me>
2023-07-19 21:08:07 -0500

    chore: fix ESLint configuration and errors (#3)
    manual fixes:
     - specs/opengsa/samgov/opportunities/Update_Attachment.yaml
     - specs/opengsa/samgov/opportunities/createAndApprove-POST.yaml
     - specs/opengsa/sustainable-facilities-tool.yaml

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+	extends: [
+		'plugin:yml/standard',
+	],
+	plugins: [
+		'eslint-plugin-yml',
+	],
+	ignorePatterns: [
+		'node_modules/**/*.{yml,yaml}',
+	],
+	rules: {
+		// specs/opengsa/samgov/entity-management.yaml
+		// specs/opengsa/samgov/exclusions.yaml
+		'yml/no-empty-mapping-value': 'off',
+		'no-trailing-spaces': 'error',
+	}
+}
